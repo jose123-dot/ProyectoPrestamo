4fb022a7b71b937d99a5330cc676ac44739c6fde
jose123-dot <72807861+jose123-dot@users.noreply.github.com>
2024-02-24 21:01:46 -0400

    inicie el frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Menu from "./components/menu/menu.jsx";
+
+function App() {
+  return (
+    <>
+      <h1>Hola Mundos Orlando</h1>;<Menu></Menu>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/menu/menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <>
+      <ul>
+        <li>Inicio</li>
+        <li>Producto</li>
+      </ul>
+    </>
+  );
+};
+
+export default Menu;
